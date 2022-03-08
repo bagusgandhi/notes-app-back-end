b55f43af28dc6ae779af9b87ac61165fe877e1fb
bagusgandhi <bagusgandhi4@gmail.com>
2022-03-09 06:41:18 +0700

    add get by id handler

--- src/handler.js
+++ src/handler.js
@@ -45,4 +45,27 @@ const getAllNotesHandler = () => ({
   },
 });
 
-module.exports = { addNoteHandler, getAllNotesHandler };
+const getNoteByIdHandler = (req, h) => {
+  const { id } = req.params;
+
+  const note = notes.filter((n) => n.id === id)[0];
+
+  if (note !== undefined) {
+    return {
+      status: 'success',
+      data: {
+        note,
+      },
+    };
+  }
+
+  const res = h.response({
+    status: 'Fail',
+    message: 'Not Found',
+  });
+
+  res.code(404);
+  return res;
+};
+
+module.exports = { addNoteHandler, getAllNotesHandler, getNoteByIdHandler };
